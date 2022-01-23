975ff3dfd6bed92b6028a1d94d5f3aa1a28834cd
slm17 <slm17slm17@gmail.com>
2022-01-23 01:20:13 -0300

    testing mobile version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const _random = document.getElementById('random')
+const _btn = document.getElementById('btn')
 
+_btn.addEventListener('click', ()=>{
+    _random.innerHTML = "";
+    fetchDataApi()
+})
 // funcion para traer datos del API
 
 function fetchDataApi(){
@@ -7,23 +12,23 @@ function fetchDataApi(){
     .then( res => res.json())
     .then(data => createPerson(data))
 }
-
+// funcion para imprimir datos en DOM
 function createPerson(p){
     // const card = document.createElement('div')
     // card.classList.add('card-body')
 _random.innerHTML +=
     `
-    <div class="d-flex flex-row justify-content-center ">
-    <div class=" mx-3 ">
+    <div id="person-Data-Container" class="d-flex flex-row justify-content-center">
+    <div class=" my-3 mx-3 ">
     <img src="${p.results[0].picture.large}" class="card-img-top rounded-circle" width="500px" alt="...">
     </div>
     
     <div >
-        <p class="card-text mb-1">Full Name: ${p.results[0].name.first} ${p.results[0].name.last}</p>
-        <p class="card-text mb-1">Age: ${p.results[0].dob.age}</p>
-        <p class="card-text mb-1">Phone: ${p.results[0].phone}</p>
-        <p class="card-text mb-1">Country: ${p.results[0].location.country}</p>
-        <p class="card-text mb-1">Email: ${p.results[0].email}</p>
+        <p class="card-text mb-1"><span>FullName:</span>  ${p.results[0].name.first} ${p.results[0].name.last}</p>
+        <p class="card-text mb-1"><span>Age:</span> ${p.results[0].dob.age}</p>
+        <p class="card-text mb-1"><span>Phone:</span> ${p.results[0].phone}</p>
+        <p class="card-text mb-1"><span>Country:</span> ${p.results[0].location.country}</p>
+        <p class="card-text mb-1"><span>Email:</span> ${p.results[0].email}</p>
     </div>
     </div>
     `
