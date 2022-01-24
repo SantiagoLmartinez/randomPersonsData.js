dda9dbfa7fa19b4131e75c32f494399558362110
slm17 <slm17slm17@gmail.com>
2022-01-23 21:15:25 -0300

    fixed spinner

--- js/main.js
+++ js/main.js
@@ -1,22 +1,51 @@
 const _random = document.getElementById('random')
-const _btn = document.getElementById('btn')
+const _btnNext = document.getElementById('btn-next')
+const _btnAdd = document.getElementById('btn-add') 
+const _showDb = document.getElementById('showDb')
+const _spinner = document.querySelector('.spinner-border')
 
-_btn.addEventListener('click', ()=>{
+let dbPerson = []
+
+// EventListener
+_btnNext.addEventListener('click', ()=>{
+    _spinner.style.display = 'block';
     _random.innerHTML = "";
     fetchDataApi()
 })
+// _btnAdd.addEventListener('click', ()=>{
+
+//     _showDb.innerHTML += dbPerson[0].FullName
+
+// })
+
+
 // funcion para traer datos del API
 
 function fetchDataApi(){
+    _spinner.style.display = 'block';
     fetch('https://randomuser.me/api/')
     .then( res => res.json())
     .then(data => createPerson(data))
+        
 }
+// funcion para capturar datos 
+class CaptureDataPerson {
+    constructor(photo,FullName, age, phone, country, email) {
+            this.photo = photo;
+            this.FullName = FullName,
+            this.age = age,
+            this.phone = phone,
+            this.country = country,
+            this.email = email;
+    }
+}
+
 // funcion para imprimir datos en DOM
 function createPerson(p){
-    // const card = document.createElement('div')
-    // card.classList.add('card-body')
-_random.innerHTML +=
+    
+    _spinner.style.display = 'none';
+
+    _random.innerHTML +=
     `
     <div id="person-Data-Container" class="d-flex flex-row justify-content-center">
     <div class=" my-3 mx-3 ">
@@ -32,14 +61,26 @@ _random.innerHTML +=
     </div>
     </div>
     `
-    
+    let photo = p.results[0].picture.large;
+    let name = p.results[0].name.first +' '+ p.results[0].name.last;
+    let age = p.results[0].dob.age;
+    let phone = p.results[0].phone;
+    let country = p.results[0].location.country;
+    let email = p.results[0].email;
 
-    // _random.innerHTML += `<p>Name: ${p.results[0].name.first}</p>`
-    // _random.innerHTML += `<p>Phone: ${p.results[0].phone}</p>`
-    // _random.innerHTML += `<p>Email: ${p.results[0].email}</p>`
-    // _random.innerHTML += `<img src="${p.results[0].picture.large}" alt="123">`
-
+    let person = new CaptureDataPerson(photo,name,age,phone,country,email)
+    addToDB(person)
+    // dbPerson.push(person);
+    // console.log(dbPerson)
+    // console.log(person)
     console.log(p.results[0])
 }
 
+function addToDB(newData){
+    dbPerson.push(newData)
+    console.log('New person data db')
+    console.log(dbPerson)
+}
+
 fetchDataApi();
+// console.log(dbPerson)
